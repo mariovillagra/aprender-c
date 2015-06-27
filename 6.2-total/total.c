4d3a22ed2e68036ddfc0a984406b94dafe76db41
Mario Villagra <marx.david92@gmail.com>
2015-06-26 20:30:26 -0400

    update

--- 6.2-total/total.c
+++ 6.2-total/total.c
@@ -10,19 +10,21 @@
  int total;       /* El total de todos los numeros */
  int item;        /* el proximo item para add a la lista */
 
- int main{
-   total = 0
-   while(1){
-     printf("Ingrese el #  a agregar\n");
-     printf("o 0 para detener: ");
+int main(){
+ total = 0;
+ while(1){
+   printf("Ingrese el #  a agregar\n");
+   printf("o 0 para detener: ");
 
-     fgets(linea, sizeof(linea), stdin);
-     sscanf(linea, "%d", &item);
+   fgets(linea, sizeof(linea), stdin);
+   sscanf(linea, "%d", &item);
 
-     if(item == 0) break;
+   if(item == 0) break;
 
-    total += item;
-    printf("El total final: %d\n",total);
-    return (0);
-   }
+   total += item;
+   printf("total: %d\n",total);
  }
+  printf("Total final %d\n", total);
+  return (0);
+
+}
